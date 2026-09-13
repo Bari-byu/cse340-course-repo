9a95b6608c81804e726f8326c738af0b053d79e8
Bari-byu <bakpee@byupathway.edu>
2026-09-13 05:31:22 +0100

    second-1

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', async (req, res) => {
 
 app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
-    console.log(organizations);
+    // console.log(organizations);
       
     const title = 'Our Partner Organizations';
     res.render('organizations', { title });
@@ -48,7 +48,7 @@ app.get('/organizations', async (req, res) => {
 
 app.get('/projects', async (req, res) => {
     const projects = await getAllProjects();
-    console.log(projects);
+    // console.log(projects);
       
     const title = 'Service Projects';
     res.render('projects', { title });
@@ -56,7 +56,7 @@ app.get('/projects', async (req, res) => {
 
 app.get('/categories', async (req, res) => {
     const categories = await getAllCategories();
-    console.log(categories);
+    // console.log(categories);
       
     const title = 'Categories';
     res.render('categories', { title });
